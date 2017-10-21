b30d5e50350810f423b5b38b08583bf6097c8a88
florin141 <abc.test@abc.com>
2017-10-21 23:02:20 +0300

    Create project structure.

---
+++ Globalmantics.Logic/UserService.cs
@@ -0,0 +1,30 @@
+﻿using Globalmantics.DAL;
+using Globalmantics.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globalmantics.Logic
+{
+	public class UserService
+	{
+		private readonly GlobalmanticsContext _context;
+
+		public UserService(GlobalmanticsContext context)
+		{
+			_context = context;
+		}
+
+		public User GetUserByEmail(string emailAddress)
+		{
+			var user = _context.Users.Add(new User
+			{
+				Email = emailAddress
+			});
+
+			return user;
+		}
+	}
+}
---
+++ Tests/Globalmantics.IntegrationTests/UserServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using Globalmantics.DAL;
+using Globalmantics.DAL.Entities;
+using Globalmantics.Logic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globalmantics.IntegrationTests
+{
+	[TestFixture]
+	public class UserServiceTests
+	{
+		[Test]
+		public void Can_create_user()
+		{
+			var context = new GlobalmanticsContext();
+			var userService = new UserService(context);
+
+			User user = userService.GetUserByEmail("test@globalmantics.com");
+			context.SaveChanges();
+
+			user.UserId.Should().NotBe(0);
+			user.Email.Should().Be("test@globalmantics.com");
+		}
+	}
+}
