468f989231c1dceef7826e31e574d8bec6f5bf88
florin141 <abc.test@abc.com>
2017-10-22 01:44:39 +0300

    Implement UsersService and CartService.

---
+++ Globalmantics.Logic/CartService.cs
@@ -0,0 +1,52 @@
+﻿using Globalmantics.DAL;
+using Globalmantics.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globalmantics.Logic
+{
+	public class CartService
+	{
+		private readonly GlobalmanticsContext _context;
+
+		public CartService(GlobalmanticsContext context)
+		{
+			_context = context;
+		}
+
+		public Cart GetCartForUser(User user)
+		{
+			var cart = _context.Carts
+				.Include(c => c.CartItems)
+				.FirstOrDefault(x => x.UserId == user.UserId);
+
+			if (cart == null)
+			{
+				cart = _context.Carts.Add(new Cart
+				{
+					UserId = user.UserId,
+					CreatedAt = DateTime.Now,
+					CartItems = new List<CartItem>()
+				});
+			}
+
+			return cart;
+		}
+
+		public void AddItemToCart(Cart cart, string sku, int quantity)
+		{
+			var catalogItem = _context.CatalogItems
+				.FirstOrDefault(x => x.Sku == sku);
+
+			cart.CartItems.Add(new CartItem
+			{
+				CatalogItem = catalogItem,
+				Quantity = quantity
+			});
+		}
+	}
+}
