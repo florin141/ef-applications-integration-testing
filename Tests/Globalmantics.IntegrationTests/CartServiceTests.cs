09d6fd1fc4d9ce148fbe22f7c026ccabca6baa10
florin141 <abc.test@abc.com>
2017-10-22 01:45:54 +0300

    Isolates integration tests.

---
+++ Tests/Globalmantics.IntegrationTests/CartServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using Globalmantics.DAL;
+using Globalmantics.DAL.Entities;
+using Globalmantics.Logic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globalmantics.IntegrationTests
+{
+	//[Isolated]
+	[TestFixture]
+	public class CartServiceTests
+	{
+		[Test]
+		public void Cart_is_initially_empty()
+		{
+			var context = new GlobalmanticsContext();
+			var userService = new UserService(context);
+			var cartService = new CartService(context);
+
+			User user = GivenUser(context, userService);
+
+			var cart = cartService.GetCartForUser(user);
+			context.SaveChanges();
+
+			cart.CartItems.Count().Should().Be(0);
+		}
+
+		private static User GivenUser(GlobalmanticsContext context, UserService userService)
+		{
+			var user = userService.GetUserByEmail($"test{Guid.NewGuid().ToString()}@globalmantics.com");
+			context.SaveChanges();
+			return user;
+		}
+
+		[Test]
+		public void Can_add_item_to_cart()
+		{
+			var context = new GlobalmanticsContext();
+			var userService = new UserService(context);
+			var cartService = new CartService(context);
+
+			User user = GivenUser(context, userService);
+
+			var cart = cartService.GetCartForUser(user);
+			context.SaveChanges();
+
+			cartService.AddItemToCart(cart, "CAFE-314", 2);
+			context.SaveChanges();
+
+			cart.CartItems.Count().Should().Be(1);
+		}
+	}
+}
---
+++ Tests/Globalmantics.IntegrationTests/IsolatedAttribute.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework.Interfaces;
+using System.Transactions;
+
+namespace Globalmantics.IntegrationTests
+{
+	public class IsolatedAttribute : Attribute, ITestAction
+	{
+		public ActionTargets Targets => ActionTargets.Test;
+
+		private TransactionScope _transactionScope;
+
+		public void AfterTest(ITest test)
+		{
+			if (_transactionScope != null)
+				_transactionScope.Dispose();
+			_transactionScope = null;
+		}
+
+		public void BeforeTest(ITest test)
+		{
+			_transactionScope = new TransactionScope(TransactionScopeOption.RequiresNew);
+		}
+	}
+}
