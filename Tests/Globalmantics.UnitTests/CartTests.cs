d7b12b1aaa1cb835962be58600fdb5ca03ced645
florin141 <abc.test@abc.com>
2017-10-22 11:28:16 +0300

    Add Globalmantics.UnitTests project.

---
+++ Tests/Globalmantics.UnitTests/CartTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Globalmantics.Domain;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Globalmantics.UnitTests
+{
+	[TestFixture]
+    public class CartTests
+    {
+		[Test]
+		public void Cart_is_initially_empty()
+		{
+			var cart = Cart.Create(0);
+
+			cart.CartItems.Count().Should().Be(0);
+		}
+    }
+}
